8cbb0fb423d2fb4d251192edd9ba0cb3407b87ff
Asurada520 <602423940@qq.com>
2020-12-14 17:44:07 +0800

    gradle programme test

---
+++ netty_lecture/src/main/java/com/jade/netty/TestServerInitializer.java
@@ -0,0 +1,18 @@
+package com.jade.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast("HttpServerCodec",new HttpServerCodec());
+        pipeline.addLast("TestServerHandler",new TestServerHandler());
+
+    }
+}
