a7b8e2904e58c90484feaf5219059ae8b81fcd2f
Asurada520 <602423940@qq.com>
2020-12-15 17:53:04 +0800

    gradle programme netty HttpRequest test

---
+++ netty_lecture/src/main/java/com/jade/netty/http/TestServerHandler.java
@@ -0,0 +1,81 @@
+package com.jade.netty.http;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.net.URI;
+
+public class TestServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+
+        System.out.println(msg.getClass());
+        System.out.println(ctx.channel().remoteAddress());
+
+        Thread.sleep(8000);
+
+        if (msg instanceof HttpRequest) {
+
+            HttpRequest httpRequest = (HttpRequest)msg;
+
+            String name = httpRequest.method().name();
+            System.out.println("请求方法名称："+ name);
+
+            String uriName = httpRequest.uri();
+            System.out.println("uriName:" + uriName);
+
+
+            URI uri = new URI(uriName);
+            String path = uri.getPath();
+
+            if("/favicon.ico".equals(path)){
+                System.out.println("请求 favicon.ico");
+                return;
+            }
+
+            ByteBuf content = Unpooled.copiedBuffer("hello world ", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            ctx.writeAndFlush(response);
+
+            ctx.channel().close();
+
+        }
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered");
+        super.channelUnregistered(ctx);
+    }
+}
--- netty_lecture/src/main/java/com/jade/netty/http/TestServerInitializer.java
+++ netty_lecture/src/main/java/com/jade/netty/http/TestServerInitializer.java
@@ -1,4 +1,4 @@
-package com.jade.netty;
+package com.jade.netty.http;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
