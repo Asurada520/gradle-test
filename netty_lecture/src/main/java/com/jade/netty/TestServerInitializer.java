1525213f8b0cc2c46fff2713079ad766b8681fa7
Asurada520 <602423940@qq.com>
2020-12-15 09:35:39 +0800

    gradle programme test

--- netty_lecture/src/main/java/com/jade/netty/TestServerInitializer.java
+++ netty_lecture/src/main/java/com/jade/netty/TestServerInitializer.java
@@ -14,6 +14,7 @@ public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
 
         pipeline.addLast("HttpServerCodec",new HttpServerCodec());
 
+
         pipeline.addLast("TestServerHandler",new TestServerHandler());
 
     }
